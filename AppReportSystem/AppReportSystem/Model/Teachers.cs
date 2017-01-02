7c3696cb5a680713d701e2b2feb21d5a535f62c6
sofifitriyah <sofi.fitriyah@basajans.com>
2017-01-02 23:44:05 +0700

    add teachers model

---
+++ AppReportSystem/AppReportSystem/Model/Teachers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppReportSystem.Model
+{
+   public class Teachers : Person
+    {
+        public string ofClass { get; set; }
+
+        public Teachers(string name, string id, DateTime birth, string gender, string kelas) : base(name, id, birth, gender)
+        {
+            this.ofClass = kelas;
+        }
+    }
+}
